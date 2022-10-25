58f05d6e26948204068f5ce640e12e210ae09b38
GONGONGONG <506419689@qq.com>
2022-10-25 16:35:45 +0800

    fix(front): 配置检查预览页查看无版本的配置实例出现系统异常 (closed #238)

--- web/src/components/ConfigDistribute/index.vue
+++ web/src/components/ConfigDistribute/index.vue
@@ -3,7 +3,7 @@
        v-test.release="'release'" v-bkloading="{ isLoading: fakeLoading }">
     <div class="distribute-above">
       <!-- 步骤显示 -->
-      <div class="steps-container">
+      <div class="steps-container" v-if="!isConfigCheck" >
         <bk-steps class="king-steps" :cur-step="curStep" :steps="steps"></bk-steps>
       </div>
       <!-- 第一步选择范围 -->
@@ -72,6 +72,7 @@
             :is-dropdown-mode="isDropdownMode"
             :selected-scope="selectedScope"
             :config-template="temp"
+            :max-height="maxHeight" 
             @update-version="updateInstanceVersion" />
         </template>
       </div>
@@ -184,6 +185,9 @@ export default {
   computed: {
     isConfigCheck() {
       return this.action === 'configCheck';
+    },
+    maxHeight() {
+      return this.isConfigCheck ? this.$store.state.pageHeight - 317 : 464;
     },
   },
   watch: {
